6ae4516f6f4e85867e856887deff8d7573921cdc
Tilen <Hli>
2019-05-27 17:02:00 +0200

    Filter fix + en za mapo (še ni dokončano)

--- platforms/browser/www/service/loadMap.js
+++ platforms/browser/www/service/loadMap.js
@@ -1,16 +1,26 @@
 //Android
-var GetMapsUri = "http://192.168.0.13:8000/maps/5ce2e12e05b4e72f600d7edd";
+//var GetMapsUri = "http://10.0.2.2:8000/maps/5ce3aa68d733861a7895f7a7";
+var GetMapsUri = "http://localhost:8000/maps/5ce3aa68d733861a7895f7a7";
+//var GetMapsUri = "http://192.168.0.13:8000/maps/5ce18f8644a71211dcd4ddd8";
 
-$(document).ready(function() {
+var storage = window.localStorage;
+
+$(document).ready(function () {
   loadMap();
 });
 
 function loadMap() {
+  if (storage.getItem("lang") == "en") {
+    console.log('en');
+  }
+  else if (storage.getItem("lang") == "sl") {
+    console.log('sl');
+  }
   $.ajax(GetMapsUri, {
-    beforeSend: function(xhr) {
+    beforeSend: function (xhr) {
       console.log('Pred');
     },
-    complete: function() {
+    complete: function () {
       console.log('Po');
     },
     contentType: 'application/json',
@@ -18,14 +28,12 @@ function loadMap() {
     async: true,
     type: 'GET',
     crossDomain: true,
-    error: function(xhr, ajaxOptions, thrownError) {
-      //alert(xhr.status);
-      //alert(xhr.responseText);
+    error: function (xhr, ajaxOptions, thrownError) {
       console.log('napaka');
       console.log(xhr.status);
       console.log(thrownError);
     },
-    success: function(data, status) {
+    success: function (data, status) {
       console.log('Rezultat iz baze');
       console.log(JSON.stringify(data));
       var map = $('#mapplic').mapplic({
@@ -40,7 +48,8 @@ function loadMap() {
         fullscreen: true,
         maxscale: 1,
         landmark: null,
-        developer: true,
+        developer: false,
+        hidenofilter: true,
       });
     }
   });
